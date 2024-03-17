4d13d5eaa9b68cd995356d8e2ec1ea36244b4479
AlexanderFRT <alexyetz@gmail.com>
2024-03-17 13:26:58 -0600

    Agregadas las pautas a desarrollar para el HTML y el JS

---
+++ app.js
@@ -0,0 +1,12 @@
+/* Las "llaves" de encriptación que fueron utilizadas son las siguientes:
+
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" */
+
+/*
+Debe funcionar solo con letras minúsculas
+No deben ser utilizadas letras con acentos ni caracteres especiales
+Debe ser posible convertir una palabra para la versión encriptada y también devolver una palabra encriptada para su versión original. */
